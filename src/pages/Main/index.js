95515bb205486e43362acf1d1d3d15f8325b8d3e
Rafael da cruz do prado <rafaeldacruzdoprado@hotmail.com>
2019-09-23 21:25:46 -0300

    Adicionando Global-Style

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -3,9 +3,7 @@ import React from 'react';
  import { Title } from './styles';
 
 export default function Main() {
-  return <Title error={false}>Main
-    <small>menor</small>
-  </Title>
+  return <Title>Hello World </Title>
 }
 
 
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -2,11 +2,9 @@ import styled from 'styled-components';
 
 export const Title = styled.h1`
   font-size: 24px;
-  color: ${ props => props.error ? 'red' : ' #7159c1' };
-  font-family: Arial, Helvetica, sans-serif;
+  color: #FFF
 
-  small {
-    font-size: 15px;
-    color:#333;
   }
+
+
 `;
