dfbb0b9da59710044463fa820b8c14dcf7cbfd5d
Rafael da cruz do prado <rafaeldacruzdoprado@hotmail.com>
2019-09-25 23:05:44 -0300

    Adicionado navegação entre paginas com parametros

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,12 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Repository from './pages/Repository';
 import Main from './pages/Main';
 
-export default function Routes (){
+export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" exact component={Main}/>
-        <Route path="/repository" component={Repository}/>
+        <Route path="/" exact component={Main} />
+        <Route path="/repository/:repository" component={Repository} />
       </Switch>
     </BrowserRouter>
   );
